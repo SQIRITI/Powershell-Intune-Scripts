9bc7f4514f09729f45338e4190f413afd6bb7a9c
SQIRITI <orava.lauri.1@gmail.com>
2025-08-26 16:35:50 +0300

    Update autopilotHWID.ps1

--- Autopilot/autopilotHWID.ps1
+++ Autopilot/autopilotHWID.ps1
@@ -1,9 +1,8 @@
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
+$dir = $PSScriptRoot
 
 New-Item -Type Directory -Path $dir\HWID
-#Set-Location -Path $dir\HWID
 $env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 Install-Script -Name Get-WindowsAutopilotInfo
 Get-WindowsAutopilotInfo -OutputFile $dir\HWID\AutopilotHWID.csv -Append 
